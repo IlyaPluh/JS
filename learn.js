852cd83aafeebbb1dbbd25ad595bb9f654f13e88
IlyaPluh <85877006+IlyaPluh@users.noreply.github.com>
2022-12-29 17:30:54 +0300

    add

--- learn.js
+++ learn.js
@@ -107,3 +107,37 @@ function count(obj) {
 
 //console.log(count(user))
 
+
+let user2 = {
+  name: "John",
+  years: 30
+};
+
+let {name, years: age, isAdmin = false} = user2;
+
+//console.log(age)
+
+
+let salaries2 = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+
+function topSalary(salary) {
+
+  let max = 0;
+  let maxName = null;
+
+  for(const [name, salary] of Object.entries(salaries)) {
+    if (max < salary) {
+      max = salary;
+      maxName = name;
+    }
+  }
+  return maxName;
+}
+
+//console.log(topSalary(salaries2))
+
+
