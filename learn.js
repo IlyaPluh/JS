89a7f90a9ff89a5c4019547e8da78dd5327242d0
IlyaPluh <85877006+IlyaPluh@users.noreply.github.com>
2023-01-06 17:11:50 +0300

    add

--- learn.js
+++ learn.js
@@ -154,3 +154,23 @@ let end = Date.now();
 //console.log( `Цикл отработал за ${end - start} миллисекунд` );
 
 
+let head = {
+  glasses: 1
+};
+let table = {
+  pen: 3,
+  __proto__: head
+};
+let bed = {
+  sheet: 1,
+  pillow: 2,
+  __proto__: table
+};
+let pockets = {
+  money: 2000,
+  __proto__: bed
+};
+
+//console.log(head.pillow)
+
+
