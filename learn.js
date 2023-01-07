d9af1ff28c3388e64773ca7509dd3d9509d72e2c
IlyaPluh <85877006+IlyaPluh@users.noreply.github.com>
2023-01-07 11:41:18 +0300

    add

--- learn.js
+++ learn.js
@@ -197,3 +197,25 @@ let obj = Object.create(null);
 let key = ("What's the key?", "__proto__");
 obj[key] = "some value";
 console.log(obj[key]);
+
+
+// ваш код, который добавляет метод dictionary.toString
+let dictionary = Object.create(null, {
+  toString: { // определяем свойство toString
+    value() { // значение -- это функция
+      return Object.keys(this).join();
+    }
+  }
+});
+
+// добавляем немного данных
+dictionary.apple = "Apple";
+dictionary.__proto__ = "test"; // здесь __proto__ -- это обычный ключ
+
+// только apple и __proto__ выведены в цикле
+for(let key in dictionary) {
+  console.log(key); // "apple", затем "__proto__"
+}
+
+// ваш метод toString в действии
+console.log(dictionary); // "apple,__proto__"
