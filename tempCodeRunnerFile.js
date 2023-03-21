5304d911ca9268c796ac54cb69e64c5906f36dc7
IlyaPluh <85877006+IlyaPluh@users.noreply.github.com>
2023-03-21 19:43:20 +0300

    array

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,4 @@
- // проверяем, что все числа от 1 до 9
-  // const isValid = numbers.every((num) => num >= 1 && num <= 9);
-  // if (!isValid) {
-  //   throw new Error('Строка должна содержать только числа от 1 до 9');
-  // }
+function filterrange(arr, min, max) {
+  return arr.filter(el => el > min && el < max);
+}
+console.log(filterrange([5, 3, 8, 1], 1, 8))
