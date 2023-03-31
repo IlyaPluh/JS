c074a3ac58b4c9d771d64ddbadac3817909b0367
IlyaPluh <85877006+IlyaPluh@users.noreply.github.com>
2023-03-31 11:56:16 +0300

    зlaywright/test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,7 +5,7 @@ test('New test grinfer', async({ page }) => {
    await page.locator('.sc-2aalce-2').click()
    await page.locator ('#email').fill('test@gmail.com')
    await page.locator ('#password').fill('Q123456778w')
-   await page.locator ('sc-p4qn9p-0').click()
+   await page.locator ('.sc-p4qn9p-0').click()
    expect (page.locator ('.sc-2aalce-2')).toBeVisible
    await page.screenshot({path: 'img/logIn.jpg'})
 })
