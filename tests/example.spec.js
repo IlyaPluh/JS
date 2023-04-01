f448013e51e8682b7a4c819a7e942b566d9c7b6a
IlyaPluh <85877006+IlyaPluh@users.noreply.github.com>
2023-04-01 17:59:10 +0300

    yarn playwright

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,10 +3,10 @@ const {test, expect} = require('@playwright/test')
 test('New test grinfer', async({ page }) => {
    await page.goto('https://grinfer.com/') 
    await page.locator('.sc-2aalce-2').click()
-   await page.locator ('#email').fill('test@gmail.com')
-   await page.locator ('#password').fill('Q123456778w')
-   await page.locator ('.sc-p4qn9p-0').click()
-   expect (page.locator ('.sc-2aalce-2')).toBeVisible
+   await page.locator('#email').fill('test@gmail.com')
+   await page.locator('#password').fill('Q123456778w')
+   await page.locator('.sc-p4qn9p-0').click()
+   expect (page.locator('.sc-2aalce-2')).toBeVisible
    await page.screenshot({path: 'img/logIn.jpg'})
 })
 
